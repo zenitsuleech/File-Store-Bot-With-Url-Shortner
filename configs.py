f151bec886beed5f4ae85300e42df04375aff350
zenitsuleech <zenistuleech@gmail.com>
2024-08-09 15:16:31 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,49 +1,25 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "25349731"))
+  API_HASH = os.environ.get("API_HASH", "58c4653460a25b1f55d22761337b5781")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6778234587:AAEVLx5PPwnVE1OFBzz45bFgK8OqInZfVfU")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "OtakuGuardianBot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002208728458"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "https://t.ly/OtakuGuardian")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "HcyY6aetVyYVnRSANwdUtha6lam7KAvb7UpgEJEC5HvOasaUdigyu9LNXEQ5")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "7019977613"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://zenistuleech:KALxIvpAixPM9RCs@cluster0.na2qk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002167928083")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002159154574"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-  ABOUT_BOT_TEXT = f"""
-This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
-
-╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
-│
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
-│
-├🔸 Language: [Python 3](https://www.python.org)
-│
-├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
-│
-╰──────[ 😎 ]───────────⍟
-"""
+  ABOUT_BOT_TEXT = f""" Otaku Guardian Store File """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
- 
- I am Super noob Please Support My Hard Work.
-
-[Donate Me](https://t.me/KingVj01)
-"""
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://t.me/mithun_naam_toh_suna_hoga)"""
   HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
-
-How to Use Bot & it's Benefits??
-
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
-
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+Hello, [{}](tg://user?id={})\n\n**Otaku Guardian Bot**
 """
